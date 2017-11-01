f77792536794d01fbc02aaa2e06eae41920cddc4
Gabriel Sanches <gsanches.dev@gmail.com>
2017-10-31 22:38:08 -0200

    Separate constants in another file

---
+++ const.go
@@ -0,0 +1,16 @@
+package httphandler
+
+const (
+	// ContentType is the respective name for the Content-Type MIME type.
+	ContentType = "Content-Type"
+	// ContentTypeJSON is the Content-Type MIME type value for JSON responses.
+	ContentTypeJSON = "application/json"
+	// ContentTypeMsgPack is the Content-Type MIME type value for MsgPack responses.
+	ContentTypeMsgPack = "application/vnd.msgpack"
+	// ContentTypeTextPlain is the Content-Type MIME type value for plain text responses.
+	ContentTypeTextPlain = "text/plain"
+	// ContentTypeXMsgPack is the Content-Type MIME type value for MsgPack responses.
+	ContentTypeXMsgPack = "application/x-msgpack"
+	// ContentTypeXML is the Content-Type MIME type value for XML responses.
+	ContentTypeXML = "application/xml"
+)
