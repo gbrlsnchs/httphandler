81743c929deb69e65e7f34fad5c5b364cf155aa6
Gabriel Sanches <gsanches.dev@gmail.com>
2017-11-11 16:45:52 -0200

    Add a benchmark test

---
+++ benchmark_test.go
@@ -0,0 +1,23 @@
+package httphandler_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	. "github.com/gbrlsnchs/httphandler"
+)
+
+func Benchmark(b *testing.B) {
+	b.ReportAllocs()
+
+	h := New(func(w http.ResponseWriter, r *http.Request) (Responder, error) {
+		return NewResponder(&struct{ test string }{test: "foobar"}, http.StatusOK), nil
+	})
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	for i := 0; i < b.N; i++ {
+		h.ServeHTTP(w, r)
+	}
+}
