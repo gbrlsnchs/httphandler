7a2873d11edaf9094bc05d603001505255817c64
Gabriel Sanches <gsanches.dev@gmail.com>
2018-02-13 15:32:03 -0200

    Reset benchmark timer

--- benchmark_test.go
+++ benchmark_test.go
@@ -9,13 +9,14 @@ import (
 )
 
 func Benchmark(b *testing.B) {
-	b.ReportAllocs()
-
 	h := New(func(w http.ResponseWriter, r *http.Request) (Responder, error) {
 		return &responderMockup{msg: "test", code: http.StatusOK}, nil
 	})
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	b.ReportAllocs()
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		h.ServeHTTP(w, r)
